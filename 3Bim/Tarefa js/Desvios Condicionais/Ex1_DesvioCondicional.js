abf8b4a3a322954ec1a9b0eb7598c349819346ae
RafaelaZilotti <164359029+RafaelaZilotti@users.noreply.github.com>
2024-09-06 14:49:58 -0300

    Desvio Condicional

---
+++ 3Bim/Tarefa js/Desvios Condicionais/Ex1_DesvioCondicional.js
@@ -0,0 +1,27 @@
+
+let parte2 = ''
+let parte3 = ''
+let parte4 = ''
+let condicao = 'condicao para comparar'
+
+let parte1 = 'parte 1'
+parte2 = parte1 + ', parte 2'
+
+
+if (condicao == 'true orig') {
+    if(condição == 'true 1'){
+        resp = 'a'
+    } else {
+        resp = 'b'
+    }
+} else{
+    if(condição == 'true 2'){
+        resp = 'c'
+        
+    } else {
+        resp = 'd'
+    }
+}
+
+parte3 = resp + ' parte 3'
+parte4 = parte3 + ' e 4'
