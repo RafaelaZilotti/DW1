876fc4964c0b18d8f24b1626d2fabb9c07d735ba
RafaelaZilotti <kimalien307@gmail.com>
2024-11-05 23:38:49 -0300

    exs 7/10 arrays

--- 4BIm/Arrays/exs/ex7.js
+++ 4BIm/Arrays/exs/ex7.js
@@ -1,1 +1,20 @@
 
+
+function mergeArrays(arr1, arr2) {
+    let tamanho1 = arr1.length
+    let tamanho2 = arr2.length
+    let junto = []
+
+    for (let i = 0; i < tamanho1; i++) {
+        junto[junto.length] = arr1[i];
+    }
+
+    for (let j = 0; j < tamanho2; j++) {
+        junto[junto.length] = arr2[j];
+    }
+
+    return junto;
+}
+
+console.log(mergeArrays([1, 2], [3, 4])); // Deve exibir: [1, 2, 3, 4]
+console.log(mergeArrays(["apple"], ["banana", "cherry"])); // Deve exibir: ["apple", "banana", "cherry"]
